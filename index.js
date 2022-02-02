640f452a4d3ea93dce7fb392d489527be4931e66
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 18:20:14 +0300

    Returns a new array containing the first two drivers from the passed-in array.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Returns a new array containing the first two drivers from the passed-in array
 const returnFirstTwoDrivers = drivers => { return drivers.slice(0, 2); }
+
+// Returns a new array containing the first two drivers from the passed-in array
+const returnLastTwoDrivers = drivers => { return drivers.slice(-2); }
