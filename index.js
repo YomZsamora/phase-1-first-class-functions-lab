81613fe17ede6280a69b1fc2687bef30796555a3
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 18:21:00 +0300

    selectingDrivers has the  function to as its first element and  function to as its last element.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const returnFirstTwoDrivers = drivers => { return drivers.slice(0, 2); }
 
 // Returns a new array containing the first two drivers from the passed-in array
 const returnLastTwoDrivers = drivers => { return drivers.slice(-2); }
+
+// selectingDrivers has the `returnFirstTwoDrivers` function to as its first element and `returnLastTwoDrivers` function to as its last element
+let selectingDrivers = [
+	returnFirstTwoDrivers, 
+	returnLastTwoDrivers
+];
